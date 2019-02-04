1c8185e7b04e9810a2bfd71ed799f47764598d28
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-02-04 21:46:17 +0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  const title = "Welcome";
+  res.render("index", { title });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
