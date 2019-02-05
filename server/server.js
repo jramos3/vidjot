bb21b37416a03d429aa90627a37ef5829ba3f6e2
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-02-05 13:53:22 +0800

    Added PUT /ideas/edit/:id route and updated validation to use express-validator lib

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const { body, validationResult } = require("express-validator/check");
 
 const { mongoose } = require("./db/mongoose");
 const { Idea } = require("./models/Idea");
 
 const app = express();
 
+//body-parse middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//method-override middleware
+app.use(methodOverride("_method"));
+
+//handlebars middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -39,38 +46,91 @@ app.get("/ideas", (req, res) => {
     .catch(err => res.status(400));
 });
 
-//POST /ideas
-app.post("/ideas", (req, res) => {
-  const errors = [];
+//GET /ideas/edit/:id
+app.get("/ideas/edit/:id", (req, res) => {
+  const { id } = req.params;
 
-  if (!req.body.title) {
-    errors.push({ errorMessage: "Please add a title." });
-  }
-
-  if (!req.body.details) {
-    errors.push({ errorMessage: "Please add some details." });
-  }
-
-  if (errors.length > 0) {
-    res.render("ideas/add", {
-      errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newIdea = {
-      title: req.body.title,
-      details: req.body.details
-    };
-
-    new Idea(newIdea)
-      .save()
-      .then(idea => {
-        res.redirect("/ideas");
-      })
-      .catch(err => res.status(400));
-  }
+  Idea.findById(id)
+    .then(idea => {
+      res.render("ideas/edit", { idea });
+    })
+    .catch(err => res.status(400));
 });
+
+//POST /ideas
+app.post(
+  "/ideas",
+  [
+    body("title", "Please add a title.")
+      .trim()
+      .isLength({ min: 1 }),
+    body("details", "Please add some details.")
+      .trim()
+      .isLength({ min: 1 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req).array();
+
+    if (errors.length) {
+      res.render("ideas/add", {
+        errors,
+        title: req.body.title,
+        details: req.body.details
+      });
+    } else {
+      const newIdea = {
+        title: req.body.title,
+        details: req.body.details
+      };
+
+      new Idea(newIdea)
+        .save()
+        .then(idea => {
+          res.redirect("/ideas");
+        })
+        .catch(err => res.status(400));
+    }
+  }
+);
+
+//PUT /idea/:id
+app.put(
+  "/ideas/:id",
+  [
+    body("title", "Please add a title.")
+      .trim()
+      .isLength({ min: 1 }),
+    body("details", "Please add some details.")
+      .trim()
+      .isLength({ min: 1 })
+  ],
+  (req, res) => {
+    const { id } = req.params;
+    const errors = validationResult(req).array();
+
+    if (errors.length) {
+      res.render("ideas/edit", {
+        errors,
+        idea: {
+          _id: id,
+          title: req.body.title,
+          details: req.body.details
+        }
+      });
+    } else {
+      const updatedIdea = {
+        title: req.body.title,
+        details: req.body.details
+      };
+
+      Idea.findByIdAndUpdate(id, { $set: updatedIdea }, { new: true })
+        .then(idea => {
+          res.redirect("/ideas");
+        })
+        .catch(err => res.status(400));
+    }
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
